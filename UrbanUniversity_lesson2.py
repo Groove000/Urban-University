cd62de91e7f85a363677cb55d27ce40e4a4cd4ab
Groove000 <d.kor.kwc@mail.ru>
2024-08-17 20:40:05 +0300

    Add files via upload

---
+++ UrbanUniversity_lesson2.py
@@ -0,0 +1,4 @@
+completed_tasks = 12
+spent_hours = 1.5
+course_name = 'Python'
+print('Курс:', course_name, ', всего задач:', completed_tasks, ', затрачено часов:', spent_hours, ', среднее время выполнения', spent_hours/completed_tasks, 'часа.')
