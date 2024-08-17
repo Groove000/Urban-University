bfa87e51e8fcb251c1c2f9a9191249873d52f4a5
Groove000 <d.kor.kwc@mail.ru>
2024-08-17 23:41:08 +0300

    Update UrbanUniversity_lesson2.py

--- UrbanUniversity_lesson2.py
+++ UrbanUniversity_lesson2.py
@@ -1,4 +1,5 @@
 completed_tasks = 12
 spent_hours = 1.5
 course_name = 'Python'
-print('Курс:', course_name, ', всего задач:', completed_tasks, ', затрачено часов:', spent_hours, ', среднее время выполнения', spent_hours/completed_tasks, 'часа.')
+one_task_time = spent_hours/completed_tasks
+print('Курс:', course_name, ', всего задач:', completed_tasks, ', затрачено часов:', spent_hours, ', среднее время выполнения', one_task_time, 'часа.')
