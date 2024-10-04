a76efa1ee55722ddf93670b9baca2f1a35f876fd
Groove000 <d.kor.kwc@mail.ru>
2024-10-04 13:34:58 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,14 +1,16 @@
-def print_params(a, b, c):
-    print(a, 25, [1,2,3])
+def print_params(a = 1, b = "Строка", c = True):
     print(a, b, c)
-    print(b)
+    print(a, b)
+    print(c)
     print()
 
-print_params(1, "Строка", True)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
 values_list = ("Дмитрий", False, 777)
 values_dict = {"a": True, "b": 4, "c": "Столбец"}
 print_params(*values_list)
 print_params(**values_dict)
 
-values_list_2 = (4, "Крест")
+values_list_2 = [54.32, "Строка"]
 print_params(*values_list_2, 42)
