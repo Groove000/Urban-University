fe94cd72dce8abaefa0772245c38dbf3f156e5f0
Groove000 <d.kor.kwc@mail.ru>
2024-11-14 12:27:09 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,18 +1,14 @@
-n = int(input("Введите число n (от 3 до 20): "))
-
-def generate_pairs(n):
-    pairs = []
-
-    for i in range(1, n):
-        for j in range(i + 1, n):
-            if i + j == n and i not in [pair for pair in pairs for number in pair]:
-                pairs.append((i, j))
-
-    return pairs
-
-pairs = generate_pairs(n)
+n = int(input("Введите целое число от 3 до 20: "))
+def get_password(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
 
 
-result = "".join([str(pair[0]) + str(pair[1]) for pair in pairs])
-
-print(f"Пароль для числа {n}: {result}")
+result = get_password(n)
+print("Пароль:", result)
